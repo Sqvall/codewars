128f14082c115db3ba710025d91f94d33d44e9a4
Sqvall <masster1987@inbox.ru>
2019-08-27 08:31:08 +0300

    commit

--- kyu_5/string_incrementer.py
+++ kyu_5/string_incrementer.py
@@ -1,3 +1,5 @@
+""" https://www.codewars.com/kata/54a91a4883a7de5d7800009c """
+
 def increment_string(strng):
     if not strng:
         return strng + '1'
---
+++ kyu_6/replace_with_alphabet_position.py
@@ -0,0 +1,22 @@
+""" https://www.codewars.com/kata/546f922b54af40e1e90001da """
+
+from string import ascii_lowercase
+
+def alphabet_position(text):
+    score = 1
+    score_dict = {}
+    for i in ascii_lowercase:
+        score_dict[i] = score
+        score += 1
+    t = text.lower()
+    out = ''
+    for i in t:
+        if i in score_dict:
+            out += repr(score_dict[i]) + " "
+    return out[0:-1]
+
+
+print(alphabet_position("The sunset sets at twelve o' clock."))
+
+# '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11'
+# '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11'
