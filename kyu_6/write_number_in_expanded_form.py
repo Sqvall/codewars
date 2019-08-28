11c29846ebb66f91cf1e74673ea763a81d79ffbf
Sqvall <masster1987@inbox.ru>
2019-08-28 21:06:54 +0300

    commit

---
+++ kyu_6/write_number_in_expanded_form.py
@@ -0,0 +1,10 @@
+""" https://www.codewars.com/kata/5842df8ccbd22792a4000245 """
+
+def expanded_form(num):
+    count = len(str(num))
+    r = []
+    for i in str(num):
+        if int(i) > 0:
+            r.append(i+'0'*(count-1))
+        count -= 1
+    return " + ".join(r)
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-def sequence_sum(begin_number, end_number, step):
-    print(sum(range(begin_number, end_number + 1, step)))
-    return sum([i for i in range(begin_number, end_number + 1, step)])
+def expanded_form(num,):
+    count = len(str(num))
+    r = []
+    for i in str(num):
+        r.append(i+'0'*(count-1))
+        # count = count - 1
+    print(r)
 
 
-print(sequence_sum(0, 6, 2))
