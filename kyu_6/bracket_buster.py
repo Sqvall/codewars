8768a4b179d4374f832c7ef996f47edfcdf15793
Sqvall <masster1987@inbox.ru>
2019-09-09 21:30:01 +0300

    commit

---
+++ kyu_6/bracket_buster.py
@@ -0,0 +1,16 @@
+""" https://www.codewars.com/kata/56ff322e79989cff16000e39 """
+
+import re
+
+
+def bracket_buster(string):
+    if isinstance(string, str):
+        result = re.findall(r'\[(.*?)\]', string)
+        return result
+    return "Take a seat on the bench."
+
+
+print(bracket_buster("Don't [pass to Ramone]"))  # ["pass to Ramone"]
+print(bracket_buster("Don't [sdfs] [pass to Ramone]"))  # ["pass to Ramone"]
+print(bracket_buster(1337))
+print(bracket_buster("[I'm] glad y'all [set it]] off"))
