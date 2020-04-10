76a0196f9d2fff5ab775278f7df5bc0918a1c398
sqvall <masster1987@inbox.ru>
2020-04-10 22:16:14 +0300

    *

---
+++ javaScript/kyu_6/detectPangram.js
@@ -0,0 +1,12 @@
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+function isPangram(string) {
+  let result = 0;
+  for (const i of alphabet) {
+    string.toString().toUpperCase().indexOf(i) !== -1 && result++;
+  }
+  return result === alphabet.length
+}
+
+isPangram('The quick brown fox jumps over the lazy dog.');
+isPangram('This is not a pangram.');
